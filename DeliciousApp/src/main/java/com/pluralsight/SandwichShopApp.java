a1bdeac652e37ca2fb07f74b2d6ffffb2c04f898
Bilenie Tilahun <bilumekb@gmail.com>
2025-05-30 12:27:10 -0500

    add my file again.

---
+++ DeliciousApp/src/main/java/com/pluralsight/SandwichShopApp.java
@@ -0,0 +1,22 @@
+package com.pluralsight;
+
+import com.pluralsight.ui.UserInterface;
+
+import static com.pluralsight.util.UiHelper.showLoadingSpinner;
+
+public class SandwichShopApp {
+    public static void main(String[] args) {
+        //Runs/Fire our app by asking and displaying the menu Home screen that asks what you want to do?
+
+        System.out.println( "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+        System.out.println("            BILENIE SANDWICH SHOP");
+        System.out.println( "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+
+        showLoadingSpinner(1000);
+
+        UserInterface ui = new UserInterface();
+        ui.homeScreen();
+
+
+    }
+}
